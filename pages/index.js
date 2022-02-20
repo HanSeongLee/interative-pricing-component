dcc3f146357e75362a8445ca8cd89d091c4b67e2
HanSeong Lee <jkhanseong@gmail.com>
2022-02-20 16:32:20 +0900

    ADD | Main page

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,75 @@
+import styles from './style.module.scss';
 import Head from 'next/head';
+import PricingFormContainer from "../containers/PricingFormContainer";
+import Container from "../components/Container";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <meta charSet="UTF-8"/>
-        <meta name="viewport"
-              content="width=device-width, initial-scale=1.0"
-        />
+      <>
+        <Head>
+          <meta charSet="UTF-8"/>
+          <meta name="viewport"
+                content="width=device-width, initial-scale=1.0"
+          />
 
-        <title>{process.env.NEXT_PUBLIC_TITLE}</title>
-        <meta name="description"
-              content={process.env.NEXT_PUBLIC_DESCRIPTION}
-        />
+          <title>{process.env.NEXT_PUBLIC_TITLE}</title>
+          <meta name="description"
+                content={process.env.NEXT_PUBLIC_DESCRIPTION}
+          />
 
-        <meta name="description"
-              content={process.env.NEXT_PUBLIC_DESCRIPTION}
-        />
+          <meta name="description"
+                content={process.env.NEXT_PUBLIC_DESCRIPTION}
+          />
 
-        <meta property="og:url"
-              content={process.env.NEXT_PUBLIC_URL}
-        />
-        <meta property="og:type"
-              content="website"
-        />
-        <meta property="og:title"
-              content={process.env.NEXT_PUBLIC_TITLE}
-        />
-        <meta property="og:description"
-              content={process.env.NEXT_PUBLIC_DESCRIPTION}
-        />
-        <meta property="og:image"
-              content={process.env.NEXT_PUBLIC_OG_IMAGE}
-        />
+          <meta property="og:url"
+                content={process.env.NEXT_PUBLIC_URL}
+          />
+          <meta property="og:type"
+                content="website"
+          />
+          <meta property="og:title"
+                content={process.env.NEXT_PUBLIC_TITLE}
+          />
+          <meta property="og:description"
+                content={process.env.NEXT_PUBLIC_DESCRIPTION}
+          />
+          <meta property="og:image"
+                content={process.env.NEXT_PUBLIC_OG_IMAGE}
+          />
 
-        <meta name="twitter:card"
-              content="summary_large_image"
-        />
-        <meta property="twitter:url"
-              content={process.env.NEXT_PUBLIC_URL}
-        />
-        <meta name="twitter:title"
-              content={process.env.NEXT_PUBLIC_TITLE}
-        />
-        <meta name="twitter:description"
-              content={process.env.NEXT_PUBLIC_DESCRIPTION}
-        />
-        <meta name="twitter:image"
-              content={process.env.NEXT_PUBLIC_OG_IMAGE}
-        />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-      </Head>
+          <meta name="twitter:card"
+                content="summary_large_image"
+          />
+          <meta property="twitter:url"
+                content={process.env.NEXT_PUBLIC_URL}
+          />
+          <meta name="twitter:title"
+                content={process.env.NEXT_PUBLIC_TITLE}
+          />
+          <meta name="twitter:description"
+                content={process.env.NEXT_PUBLIC_DESCRIPTION}
+          />
+          <meta name="twitter:image"
+                content={process.env.NEXT_PUBLIC_OG_IMAGE}
+          />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png"/>
+        </Head>
 
-      <main>
-      </main>
-    </>
-  )
+        <main className={styles.main}>
+          <Container className={styles.headingContainer}>
+            <h1 className={styles.title}>
+              Simple, traffic-based pricing
+            </h1>
+            <p className={styles.description}>
+              Sign-up for our 30-day trial.
+              No credit card required.
+            </p>
+          </Container>
+
+          <Container>
+            <PricingFormContainer/>
+          </Container>
+        </main>
+      </>
+  );
 }
