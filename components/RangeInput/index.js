1c5de617340cdbe08e553f4be2cda44c3ed0198d
HanSeong Lee <jkhanseong@gmail.com>
2022-02-20 22:46:34 +0900

    ADD | Hover animation and drag event

--- components/RangeInput/index.js
+++ components/RangeInput/index.js
@@ -1,17 +1,30 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import styles from './style.module.scss';
+import cn from "classnames";
 
 const RangeInput = ({
                         name, min = 0, max = 1, value,
                         onChange, ...props
                     }) => {
+    const [drag, setDrag] = useState(false);
+
+    const onDragEnter = useCallback((e) => {
+        setDrag(true);
+    }, []);
+
+    const onDragExit = useCallback((e) => {
+        setDrag(false);
+    }, []);
+
     return (
         <div className={styles.rangeInputWrapper}
              style={{
                  '--progress-width': `${(value / max) * 100}%`,
              }}
         >
-            <input className={styles.rangeInput}
+            <input className={cn(styles.rangeInput, {
+                [styles.drag]: drag,
+            })}
                    name={name}
                    type={'range'}
                    min={min}
@@ -19,6 +32,8 @@ const RangeInput = ({
                    value={value}
                    onChange={onChange}
                    aria-label={name}
+                   onMouseDown={onDragEnter}
+                   onMouseUp={onDragExit}
                    {...props}
             />
         </div>
